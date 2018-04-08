331761489ac3f8f11bce617b3a193400077ef06b
Bartosz Gajda <bg@bartoszgajda.com>
2018-04-08 22:07:39 +0100

    Added user edit endpoint.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,6 +36,46 @@ class UserController extends Controller
     }
 
     /**
+     * Edit user entity
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit(Request $request, $id)
+    {
+        $user = User::find($id);
+        if(!$user) {
+            return Response::create([], 404);
+        }
+
+        if($request->has('username')) {
+            $user->Username = $request->input('username');
+        }
+        if($request->has('email')) {
+            $user->Email = $request->input('email');
+        }
+        if($request->has('firstname')) {
+            $user->Firstname = $request->input('firstname');
+        }
+        if($request->has('surname')) {
+            $user->Surname = $request->input('surname');
+        }
+        if($request->has('password')) {
+            $user->Password = $request->input('password');
+        }
+        if($request->has('adminstatus')) {
+            $user->AdminStatus = $request->input('adminstatus');
+        }
+
+        if($user->save()) {
+            return Response::create([], 200);
+        } else {
+            return Response::create([], 500);
+        }
+    }
+
+    /**
      * Check if user exists and credentials are matching
      *
      * @param  Request  $request
